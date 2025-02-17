b13111b8c6b52342e870555904eb58e5897babad
Ehaab Basil <54548391+ehaabbasil@users.noreply.github.com>
2025-02-16 19:00:07 -0500

    First Draft of Website
    Personal Website,
    ---added the main page
    --fixed the  styles with dynamic dark and light them
    -- containers set up for texts and themes
    --currently in progress Projects section

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+
+
+function App() {  
+
+  return ( 
+  <>
+  <Hero />
+  <Projects />
+  </>
+  ); 
+}
+
+export default App
+ 
